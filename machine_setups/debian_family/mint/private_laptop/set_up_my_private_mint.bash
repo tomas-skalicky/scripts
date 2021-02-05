0ae6b6c7d1763049ff15c6d23c1cbf470f083b8a
Tomas Skalicky <skalicky.tomas@gmail.com>
2021-02-05 04:58:03 +0100

    JAVA_HOME holding a reference to AdoptOpenJDK added to PATH

--- machine_setups/debian_family/mint/private_laptop/set_up_my_private_mint.bash
+++ machine_setups/debian_family/mint/private_laptop/set_up_my_private_mint.bash
@@ -42,7 +42,6 @@ main() {
     source ../shared/roles/secured_functions
     source ../shared/roles/base
     source ../shared/roles/snap
-    source ../shared/roles/openjdk
     source ../shared/roles/jvm_software_development_kit java 15.0.2.hs-adpt
     source ../shared/roles/jvm_software_development_kit groovy
     source ../shared/roles/jvm_software_development_kit maven
